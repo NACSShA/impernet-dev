f158097d7aa75fe2fba0fb97b937fde67fe755c3
NACSShA <notacountryssha@gmail.com>
2026-06-19 17:18:43 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,1 @@
-import telebot
-from telebot import types
 
-from config import TOKEN
-from database import init_db
-
-bot = telebot.TeleBot(TOKEN, parse_mode="HTML")
-
-init_db()
-
-print("IMPERNET запущен.")
-
-bot.infinity_polling(skip_pending=True)
